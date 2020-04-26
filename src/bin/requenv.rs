316022e243cdf892d29555732ecf946e58e47fcb
Thomas Herzog <thomas-code@poto.cafe>
2020-04-26 23:42:07 +0200

    move argument parsing to binary and use clap

--- src/bin/requenv.rs
+++ src/bin/requenv.rs
@@ -3,50 +3,153 @@
 // SPDX-License-Identifier: OSL-3.0
 
 use std::os::unix::process::CommandExt as _;
+use std::convert::TryFrom as _;
 use std::process::Command;
 
-use reqenv::command_from_args;
+use clap::{Arg, App, value_t};
+
+use reqenv::{SizePostfix, build_command, Options};
 
 fn main() {
-	let args: Vec<_> = std::env::args_os().skip(1).collect();
 	
-	let separator_idx_res: Option<usize> = args
-		.iter()
-		.enumerate()
-		.find_map(|(i, s)| if s == "--" { Some(i) } else { None } );
+	let num_cpus = u8::try_from(num_cpus::get()).unwrap_or(u8::max_value());
 	
-	let separator_idx = separator_idx_res.unwrap_or(0);
+	let matches = App::new("reqenv")
+		.version("0.1.0")
+		.author("Thomas Herzog")
+		.arg(
+			Arg::with_name("install-polkit-rule")
+				.long("install-polkit-rule")
+				.help("Install a polkit rule to avoid authentication (requires root)")
+				.takes_value(false)
+		)
+		.arg(
+			Arg::with_name("cpus")
+				.long("cpus")
+				.value_name("N")
+				.help("Number of CPU cores to use")
+				.takes_value(true)
+		)
+		.arg(
+			Arg::with_name("cpu-speed")
+				.long("cpu-speed")
+				.value_name("percent")
+				.help("CPU utilisation in percent (integer)")
+				.takes_value(true)
+		)
+		.arg(
+			Arg::with_name("memory")
+				.long("memory")
+				.value_name("amount (K/M/G/T)")
+				.help("Amount of virtual memory to provide")
+				.takes_value(true)
+				
+		)
+		.arg(
+			Arg::with_name("command")
+				.multiple(true)
+				.help("Command to run")
+				.last(true)
+		)
+		.get_matches();
 	
-	let has_dashes = separator_idx_res.is_some();
-	let command_skip = if has_dashes { 1 } else { 0 };
+	fn usage_exit(param: &str, matches: clap::ArgMatches<'_>) -> ! {
+		let usage = matches.usage();
+		eprint!("Invalid parameter `{}`. ", param);
+		eprintln!("Use `--help` to get an overview of the options");
+		eprintln!("{}", usage);
+		std::process::exit(1)
+	};
 	
-	let (options, command) = args.split_at(separator_idx);
-	
-	let command: Vec<String> = command
-		.into_iter()
-		.skip(command_skip)
-		.map(|s| s.clone().into_string().unwrap())
-		.collect();
-	
-	if command.is_empty() {
-		eprintln!("ERROR: Must provide a command");
-		std::process::exit(1);
+	if matches.is_present("install-polkit-rule") {
+		install_polkit_rule();
 	}
 	
-	let num_cpus = num_cpus::get();
+	let cpus_raw = matches.value_of("cpus");
+	let cpu_speed = value_t!(matches, "cpu-speed", u8)
+		.ok()
+		.map(|s| s.min(100));
+	let memory_raw = matches.value_of("memory");
+	let command = matches.values_of_lossy("command");
 	
-	match command_from_args(num_cpus as u8, options.to_vec(), &command) {
-		Ok(command) => {
-			let error = Command::new(command.program)
-				.args(command.arguments)
-				.exec();
-			
-			eprintln!("Unable to spawn program: {}", error);
-			std::process::exit(1);
-		},
-		Err(err) => {
-			eprintln!("Error: {}", err);
-			std::process::exit(1);
-		},
+	let cpus = if let Some(raw) = cpus_raw {
+		if let Ok(res) = raw.parse::<u8>() {
+			Some(res.max(1).min(num_cpus))
+		} else {
+			usage_exit("--cpus", matches)
+		}
+	} else {
+		None
+	};
+	
+	let memory = if let Some(raw) = memory_raw {
+		if let Ok(res) = parse_memory(raw) {
+			Some(res)
+		} else {
+			usage_exit("--memory", matches)
+		}
+	} else {
+		None
+	};
+	
+	let command = if let Some(raw) = command {
+		raw
+	} else {
+		usage_exit("<<command>>", matches)
+	};
+	
+	if cpus.is_none() && cpu_speed.is_none() && memory.is_none() {
+		todo!("loading from a config file is not yet supported")
+	}
+	
+	let options = Options {
+		cpus,
+		cpu_speed,
+		memory,
+	};
+	
+	let command = build_command(num_cpus, options, &command);
+	
+	// the `.exec()` replaces the process memory with the new command's one
+	// so this should not return, and if it does there was an error.
+	let error = Command::new(command.program)
+		.args(command.arguments)
+		.exec();
+	
+	eprintln!("Unable to spawn program: {}", error);
+	std::process::exit(1);
+}
+
+const POLKIT_RULE: &str = include_str!("../../assets/99-reqenv.rules");
+
+fn install_polkit_rule() -> ! {
+	const PATH: &str = "/etc/polkit-1/rules.d/99-reqenv.rules";
+	
+	std::fs::write(PATH, POLKIT_RULE)
+		.expect("Unable to write file. Are you root/sudo?");
+	std::process::exit(0)
+}
+
+fn parse_memory(s: &str) -> Result<(u32, SizePostfix), std::num::ParseIntError> {
+	if s.ends_with('K') {
+		let s = s.trim_end_matches('K');
+		let num = s.parse()?;
+		Ok((num, SizePostfix::Kilo))
+	} else if s.ends_with('M') {
+		let s = s.trim_end_matches('M');
+		let num = s.parse()?;
+		Ok((num, SizePostfix::Mega))
+	} else if s.ends_with('G') {
+		let s = s.trim_end_matches('G');
+		let num = s.parse()?;
+		Ok((num, SizePostfix::Giga))
+	} else if s.ends_with('T') {
+		let s = s.trim_end_matches('T');
+		let num = s.parse()?;
+		Ok((num, SizePostfix::Tera))
+	} else {
+		let num = s.parse()?;
+		Ok((num, SizePostfix::None))
 	}
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 use contracts::*;
 
 #[derive(Copy, Clone, Debug)]
-enum SizePostfix {
+pub enum SizePostfix {
 	None,
 	Kilo,
 	Mega,
@@ -27,10 +27,10 @@ impl std::string::ToString for SizePostfix {
 }
 
 #[derive(Copy, Clone, Debug)]
-struct Options {
-	cpus: Option<u8>,
-	cpu_speed: Option<u8>,
-	memory: Option<(u32, SizePostfix)>,
+pub struct Options {
+	pub cpus: Option<u8>,
+	pub cpu_speed: Option<u8>,
+	pub memory: Option<(u32, SizePostfix)>,
 }
 
 impl Options {
@@ -60,74 +60,6 @@ pub struct Command {
 	pub arguments: Vec<String>,
 }
 
-// This contract can't be enabled because it generates invalid code with the
-// `impl Trait` return type used here.
-//
-// #[pre(!command.is_empty(), "must provide a command to execute")]
-/// Build a [`Command`](struct.Command.html) from command line arguments.
-///
-/// - `num_cpus` are the number of CPUs available on the system.
-///   This is needed to appropriately scale the cpu quota appropriately.
-/// - `args` are the options of the current program that affect the restrictions
-///   on the program that should be executed.
-/// - `command` are the program-path and arguments of the program that should be
-///   executed.
-///
-/// Errors can occur when invalid `args` are supplied.
-pub fn command_from_args(
-	num_cpus: u8,
-	args: Vec<std::ffi::OsString>,
-	command: &[String],
-) -> Result<Command, impl std::fmt::Display + std::fmt::Debug> {
-	
-	macro_rules! opt {
-		($e:expr) => {
-			match $e {
-				Result::Ok(val) => val,
-				Result::Err(err) => return Err(err)
-			}
-		};
-	};
-	
-	let mut args = pico_args::Arguments::from_vec(args);
-	let cpus: Option<u8> = opt!(args.opt_value_from_str("--cpus"));
-	let cpu_speed: Option<u8> = opt!(args.opt_value_from_str("--cpu-speed"));
-	let memory = opt!(args.opt_value_from_fn("--memory", parse_memory));
-	
-	opt!(args.finish());
-	
-	let opts = Options {
-		cpus: cpus.map(|c| c.max(1).min(num_cpus)),
-		cpu_speed: cpu_speed.map(|s| s.min(100)),
-		memory,
-	};
-	
-	Ok(build_command(num_cpus, opts, &command))
-}
-
-fn parse_memory(s: &str) -> Result<(u32, SizePostfix), std::num::ParseIntError> {
-	if s.ends_with('K') {
-		let s = s.trim_end_matches('K');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Kilo))
-	} else if s.ends_with('M') {
-		let s = s.trim_end_matches('M');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Mega))
-	} else if s.ends_with('G') {
-		let s = s.trim_end_matches('G');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Giga))
-	} else if s.ends_with('T') {
-		let s = s.trim_end_matches('T');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Tera))
-	} else {
-		let num = s.parse()?;
-		Ok((num, SizePostfix::None))
-	}
-}
-
 #[pre(num_cpus > 0)]
 #[pre(opts.invariant())]
 #[pre(!command.is_empty(), "must provide a command to execute")]
@@ -139,7 +71,7 @@ fn parse_memory(s: &str) -> Result<(u32, SizePostfix), std::num::ParseIntError> 
 // #[post(opts.memory.is_some()
 // 	==> ret.arguments.first(|e| e.starts_with("MemoryMax")).is_some()
 // )]
-fn build_command(
+pub fn build_command(
 	num_cpus: u8,
 	opts: Options,
 	command: &[String],
