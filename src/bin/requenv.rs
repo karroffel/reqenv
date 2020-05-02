370b6a4061ef6a2ebad1f522c6a22800731b66b0
Thomas Herzog <thomas-code@poto.cafe>
2020-05-02 13:42:54 +0200

    ran `cargo fmt` and fixed `clippy` warnings

--- src/bin/requenv.rs
+++ src/bin/requenv.rs
@@ -2,150 +2,142 @@
 //
 // SPDX-License-Identifier: OSL-3.0
 
-use std::os::unix::process::CommandExt as _;
 use std::convert::TryFrom as _;
+use std::os::unix::process::CommandExt as _;
 use std::process::Command;
 
-use clap::{Arg, App, value_t};
+use clap::{value_t, App, Arg};
 
-use reqenv::{SizePostfix, build_command, Options};
+use reqenv::{build_command, Options, SizePostfix};
 
 fn main() {
-	
-	let num_cpus = u8::try_from(num_cpus::get()).unwrap_or(u8::max_value());
-	
-	let matches = App::new("reqenv")
-		.version("0.1.0")
-		.author("Thomas Herzog")
-		.arg(
-			Arg::with_name("install-polkit-rule")
-				.long("install-polkit-rule")
-				.help("Install a polkit rule to avoid authentication (requires root)")
-				.takes_value(false)
-		)
-		.arg(
-			Arg::with_name("cpus")
-				.long("cpus")
-				.value_name("N")
-				.help("Number of CPU cores to use")
-				.takes_value(true)
-		)
-		.arg(
-			Arg::with_name("cpu-speed")
-				.long("cpu-speed")
-				.value_name("percent")
-				.help("CPU utilisation in percent (integer)")
-				.takes_value(true)
-		)
-		.arg(
-			Arg::with_name("memory")
-				.long("memory")
-				.value_name("amount (K/M/G/T)")
-				.help("Amount of virtual memory to provide")
-				.takes_value(true)
-				
-		)
-		.arg(
-			Arg::with_name("command")
-				.multiple(true)
-				.help("Command to run")
-				.last(true)
-		)
-		.get_matches();
-	
-	fn usage_exit(param: &str, matches: clap::ArgMatches<'_>) -> ! {
-		let usage = matches.usage();
-		eprint!("Invalid parameter `{}`. ", param);
-		eprintln!("Use `--help` to get an overview of the options");
-		eprintln!("{}", usage);
-		std::process::exit(1)
-	};
-	
-	if matches.is_present("install-polkit-rule") {
-		install_polkit_rule();
-	}
-	
-	let cpus_raw = matches.value_of("cpus");
-	let cpu_speed = value_t!(matches, "cpu-speed", u8)
-		.ok()
-		.map(|s| s.min(100));
-	let memory_raw = matches.value_of("memory");
-	let command = matches.values_of_lossy("command");
-	
-	let cpus = if let Some(raw) = cpus_raw {
-		if let Ok(res) = raw.parse::<u8>() {
-			Some(res.max(1).min(num_cpus))
-		} else {
-			usage_exit("--cpus", matches)
-		}
-	} else {
-		None
-	};
-	
-	let memory = if let Some(raw) = memory_raw {
-		if let Ok(res) = parse_memory(raw) {
-			Some(res)
-		} else {
-			usage_exit("--memory", matches)
-		}
-	} else {
-		None
-	};
-	
-	let command = if let Some(raw) = command {
-		raw
-	} else {
-		usage_exit("<<command>>", matches)
-	};
-	
-	let options = Options {
-		cpus,
-		cpu_speed,
-		memory,
-	};
-	
-	let command = build_command(num_cpus, options, &command);
-	
-	// the `.exec()` replaces the process memory with the new command's one
-	// so this should not return, and if it does there was an error.
-	let error = Command::new(command.program)
-		.args(command.arguments)
-		.exec();
-	
-	eprintln!("Unable to spawn program: {}", error);
-	std::process::exit(1);
+    let num_cpus = u8::try_from(num_cpus::get()).unwrap_or(u8::max_value());
+
+    let matches = App::new("reqenv")
+        .version("0.1.0")
+        .author("Thomas Herzog")
+        .arg(
+            Arg::with_name("install-polkit-rule")
+                .long("install-polkit-rule")
+                .help("Install a polkit rule to avoid authentication (requires root)")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("cpus")
+                .long("cpus")
+                .value_name("N")
+                .help("Number of CPU cores to use")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("cpu-speed")
+                .long("cpu-speed")
+                .value_name("percent")
+                .help("CPU utilisation in percent (integer)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("memory")
+                .long("memory")
+                .value_name("amount (K/M/G/T)")
+                .help("Amount of virtual memory to provide")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("command")
+                .multiple(true)
+                .help("Command to run")
+                .last(true),
+        )
+        .get_matches();
+
+    fn usage_exit(param: &str, matches: clap::ArgMatches<'_>) -> ! {
+        let usage = matches.usage();
+        eprint!("Invalid parameter `{}`. ", param);
+        eprintln!("Use `--help` to get an overview of the options");
+        eprintln!("{}", usage);
+        std::process::exit(1)
+    };
+
+    if matches.is_present("install-polkit-rule") {
+        install_polkit_rule();
+    }
+
+    let cpus_raw = matches.value_of("cpus");
+    let cpu_speed = value_t!(matches, "cpu-speed", u8).ok().map(|s| s.min(100));
+    let memory_raw = matches.value_of("memory");
+    let command = matches.values_of_lossy("command");
+
+    let cpus = if let Some(raw) = cpus_raw {
+        if let Ok(res) = raw.parse::<u8>() {
+            Some(res.max(1).min(num_cpus))
+        } else {
+            usage_exit("--cpus", matches)
+        }
+    } else {
+        None
+    };
+
+    let memory = if let Some(raw) = memory_raw {
+        if let Ok(res) = parse_memory(raw) {
+            Some(res)
+        } else {
+            usage_exit("--memory", matches)
+        }
+    } else {
+        None
+    };
+
+    let command = if let Some(raw) = command {
+        raw
+    } else {
+        usage_exit("<<command>>", matches)
+    };
+
+    let options = Options {
+        cpus,
+        cpu_speed,
+        memory,
+    };
+
+    let command = build_command(num_cpus, options, &command);
+
+    // the `.exec()` replaces the process memory with the new command's one
+    // so this should not return, and if it does there was an error.
+    let error = Command::new(command.program).args(command.arguments).exec();
+
+    eprintln!("Unable to spawn program: {}", error);
+    std::process::exit(1);
 }
 
 const POLKIT_RULE: &str = include_str!("../../assets/99-reqenv.rules");
 
 fn install_polkit_rule() -> ! {
-	const PATH: &str = "/etc/polkit-1/rules.d/99-reqenv.rules";
-	
-	std::fs::write(PATH, POLKIT_RULE)
-		.expect("Unable to write file. Are you root/sudo?");
-	std::process::exit(0)
+    const PATH: &str = "/etc/polkit-1/rules.d/99-reqenv.rules";
+
+    std::fs::write(PATH, POLKIT_RULE).expect("Unable to write file. Are you root/sudo?");
+    std::process::exit(0)
 }
 
 fn parse_memory(s: &str) -> Result<(u32, SizePostfix), std::num::ParseIntError> {
-	if s.ends_with('K') {
-		let s = s.trim_end_matches('K');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Kilo))
-	} else if s.ends_with('M') {
-		let s = s.trim_end_matches('M');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Mega))
-	} else if s.ends_with('G') {
-		let s = s.trim_end_matches('G');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Giga))
-	} else if s.ends_with('T') {
-		let s = s.trim_end_matches('T');
-		let num = s.parse()?;
-		Ok((num, SizePostfix::Tera))
-	} else {
-		let num = s.parse()?;
-		Ok((num, SizePostfix::None))
-	}
+    if s.ends_with('K') {
+        let s = s.trim_end_matches('K');
+        let num = s.parse()?;
+        Ok((num, SizePostfix::Kilo))
+    } else if s.ends_with('M') {
+        let s = s.trim_end_matches('M');
+        let num = s.parse()?;
+        Ok((num, SizePostfix::Mega))
+    } else if s.ends_with('G') {
+        let s = s.trim_end_matches('G');
+        let num = s.parse()?;
+        Ok((num, SizePostfix::Giga))
+    } else if s.ends_with('T') {
+        let s = s.trim_end_matches('T');
+        let num = s.parse()?;
+        Ok((num, SizePostfix::Tera))
+    } else {
+        let num = s.parse()?;
+        Ok((num, SizePostfix::None))
+    }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -6,45 +6,42 @@ use contracts::*;
 
 #[derive(Copy, Clone, Debug)]
 pub enum SizePostfix {
-	None,
-	Kilo,
-	Mega,
-	Giga,
-	Tera,
+    None,
+    Kilo,
+    Mega,
+    Giga,
+    Tera,
 }
 
 impl std::string::ToString for SizePostfix {
-	fn to_string(&self) -> String {
-		let lit = match self {
-			SizePostfix::None => "",
-			SizePostfix::Kilo => "K",
-			SizePostfix::Mega => "M",
-			SizePostfix::Giga => "G",
-			SizePostfix::Tera => "T",
-		};
-		lit.to_string()
-	}
+    fn to_string(&self) -> String {
+        let lit = match self {
+            SizePostfix::None => "",
+            SizePostfix::Kilo => "K",
+            SizePostfix::Mega => "M",
+            SizePostfix::Giga => "G",
+            SizePostfix::Tera => "T",
+        };
+        lit.to_string()
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
 pub struct Options {
-	pub cpus: Option<u8>,
-	pub cpu_speed: Option<u8>,
-	pub memory: Option<(u32, SizePostfix)>,
+    pub cpus: Option<u8>,
+    pub cpu_speed: Option<u8>,
+    pub memory: Option<(u32, SizePostfix)>,
 }
 
 impl Options {
-	fn invariant(&self) -> bool {
-		assert!(
-			self.cpus.unwrap_or(1) > 0,
-			"CPU count must be non-zero",
-		);
-		assert!(
-			self.cpu_speed.unwrap_or(100) <= 100,
-			"CPU speed must not exceed 100%",
-		);
-		true
-	}
+    fn invariant(&self) -> bool {
+        assert!(self.cpus.unwrap_or(1) > 0, "CPU count must be non-zero",);
+        assert!(
+            self.cpu_speed.unwrap_or(100) <= 100,
+            "CPU speed must not exceed 100%",
+        );
+        true
+    }
 }
 
 /// The command that should be executed by the system to apply the restrictions
@@ -54,10 +51,10 @@ impl Options {
 /// [cmd]: https://doc.rust-lang.org/std/process/struct.Command.html
 #[derive(Debug, Clone)]
 pub struct Command {
-	/// The program to execute.
-	pub program: String,
-	/// The arguments of the program.
-	pub arguments: Vec<String>,
+    /// The program to execute.
+    pub program: String,
+    /// The arguments of the program.
+    pub arguments: Vec<String>,
 }
 
 #[pre(num_cpus > 0)]
@@ -66,74 +63,72 @@ pub struct Command {
 #[post(ret.arguments.ends_with(command), "command must be preserved")]
 #[post(opts.cpus.is_some() ==> ret.arguments.contains(&"taskset".into()))]
 #[post(opts.cpus.or(opts.cpu_speed).is_some()
-	==> ret.arguments.iter().find(|e| e.starts_with("CPUQuota")).is_some()
+	==> ret.arguments.iter().any(|e| e.starts_with("CPUQuota"))
 )]
 #[post(opts.memory.is_some()
-	==> ret.arguments.iter().find(|e| e.starts_with("MemoryMax")).is_some()
+	==> ret.arguments.iter().any(|e| e.starts_with("MemoryMax"))
 )]
-pub fn build_command(
-	num_cpus: u8,
-	opts: Options,
-	command: &[String],
-) -> Command {
-	let program = "systemd-run".to_string();
-	let mut args = vec!["--quiet".to_string(), "--scope".to_string()];
-	
-	if let Some((size, postfix)) = opts.memory {
-		args.push("-p".to_string());
-		args.push(format!("MemoryMax={}{}", size, postfix.to_string()));
-	}
-	
-	let speed = match (opts.cpus, opts.cpu_speed) {
-		(None, None) => None,
-		(None, Some(speed)) => {
-			let cpus = u16::from(num_cpus);
-			let total_speed: u16 = cpus * u16::from(speed);
-			Some(total_speed)
-		},
-		(Some(cpus), None) => {
-			let cpus = u16::from(cpus.min(num_cpus));
-			let speed: u16 = 100;
-			let total_speed = cpus * speed;
-			Some(total_speed)
-		},
-		(Some(cpus), Some(speed)) => {
-			let cpus = u16::from(cpus.min(num_cpus));
-			let speed = u16::from(speed);
-			let total_speed = cpus * speed;
-			Some(total_speed)
-		},
-	};
-	
-	if let Some(speed) = speed {
-		args.push("-p".to_string());
-		args.push(format!("CPUQuota={}%", speed));
-	}
-	
-	if let Some(cpus) = opts.cpus {
-		let cpus = cpus.min(num_cpus);
-		
-		let cpu_list: String = select_cpus(num_cpus, cpus)
-			.map(|n| n.to_string())
-			.collect::<Vec<_>>()
-			.join(",");
-		
-		args.push("taskset".to_string());
-		args.push("-c".to_string());
-		args.push(cpu_list);
-	}
-	
-	args.extend_from_slice(command);
-	
-	Command { program, arguments: args }
-}
+pub fn build_command(num_cpus: u8, opts: Options, command: &[String]) -> Command {
+    let program = "systemd-run".to_string();
+    let mut args = vec!["--quiet".to_string(), "--scope".to_string()];
 
+    if let Some((size, postfix)) = opts.memory {
+        args.push("-p".to_string());
+        args.push(format!("MemoryMax={}{}", size, postfix.to_string()));
+    }
+
+    let speed = match (opts.cpus, opts.cpu_speed) {
+        (None, None) => None,
+        (None, Some(speed)) => {
+            let cpus = u16::from(num_cpus);
+            let total_speed: u16 = cpus * u16::from(speed);
+            Some(total_speed)
+        }
+        (Some(cpus), None) => {
+            let cpus = u16::from(cpus.min(num_cpus));
+            let speed: u16 = 100;
+            let total_speed = cpus * speed;
+            Some(total_speed)
+        }
+        (Some(cpus), Some(speed)) => {
+            let cpus = u16::from(cpus.min(num_cpus));
+            let speed = u16::from(speed);
+            let total_speed = cpus * speed;
+            Some(total_speed)
+        }
+    };
+
+    if let Some(speed) = speed {
+        args.push("-p".to_string());
+        args.push(format!("CPUQuota={}%", speed));
+    }
+
+    if let Some(cpus) = opts.cpus {
+        let cpus = cpus.min(num_cpus);
+
+        let cpu_list: String = select_cpus(num_cpus, cpus)
+            .map(|n| n.to_string())
+            .collect::<Vec<_>>()
+            .join(",");
+
+        args.push("taskset".to_string());
+        args.push("-c".to_string());
+        args.push(cpu_list);
+    }
+
+    args.extend_from_slice(command);
+
+    Command {
+        program,
+        arguments: args,
+    }
+}
 
 #[pre(num_cpus > 0, used > 0)]
 #[pre(num_cpus >= used, "must have enough cores available")]
 #[post(ret.end == num_cpus)]
 #[post((ret.end - ret.start) == used, "will return a range with enough cores")]
 fn select_cpus(num_cpus: u8, used: u8) -> core::ops::Range<u8> {
-	let first = num_cpus - used;
-	first..num_cpus
+    let first = num_cpus - used;
+    first..num_cpus
 }
